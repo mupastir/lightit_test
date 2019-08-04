5e636bb40d9da43af39e8ac3b3267ef45f3458c5
mupastir <mupastir@gmail.com>
2019-08-04 21:26:46 +0300

    init templates

--- core/app.py
+++ core/app.py
@@ -1,19 +1,33 @@
 from flask import Flask
-from flask_redis import FlaskRedis
-
+from flask import g
 from core.config import runtime_config
+
+import sqlite3
+import os
 
 from core.routes.cipher import Cipher
 
+
+DATABASE = os.environ.get('DATABASE', '/core/models/database.db')
 
 app = Flask(__name__)
 app.config.from_object(runtime_config())
 
 
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
 @app.route('/')
 def index():
-    return Cipher()
-
-
-redis_client = FlaskRedis()
-redis_client.init_app(app)
+    return Cipher().main_page()
--- core/routes/cipher.py
+++ core/routes/cipher.py
@@ -1,2 +1,11 @@
+from flask import render_template
+
+
 class Cipher:
-    pass
+
+    def __init__(self):
+        self.main_page_legend = 'Cesar cipher site. Please choose rotate and case: code or decode <br>' \
+                                'After that enter text in the left field'
+
+    def main_page(self):
+        return render_template("index.html", legend=self.main_page_legend)
